9d6ece0ed6f5dc980c3a0e293a9ce9fe47351a98
Lasse <lasse@haslev.no>
2017-12-26 12:32:07 +0100

    Added background mixin test.

---
+++ test/Background.js
@@ -0,0 +1,64 @@
+import test from 'ava';
+import sass from 'node-sass';
+import path from 'path';
+
+var renderSass = function( content ) {
+    var result = sass.renderSync({
+        includePaths: [
+            path.resolve( '.' ),
+        ],
+        outputStyle: 'compressed',
+        data: content,
+    });
+    return result.css.toString().slice( 0, -1 );
+}
+
+test( 'it can generate style to add background image', t => {
+
+    var sass = renderSass( `
+        @import 'src/main.scss';
+        .test {
+            @include background-image;
+        }
+    ` );
+
+    t.is( sass, '.test{background-size:contain;background-repeat:no-repeat;background-position:center}' );
+
+} );
+
+test( 'it can set a background image', t => {
+
+    var sass = renderSass( `
+        @import 'src/main.scss';
+        .test {
+            @include background-image( 'image.png' );
+        }
+    ` );
+
+    t.is( sass, '.test{background:"image.png";background-size:contain;background-repeat:no-repeat;background-position:center}' );
+
+} );
+
+test( 'it can change the background size', t => {
+
+    var sass = renderSass( `
+        @import 'src/main.scss';
+        .test {
+            @include background-image( null, cover );
+        }
+    ` );
+
+    t.is( sass, '.test{background-size:cover;background-repeat:no-repeat;background-position:center}' );
+
+} );
+
+test( 'it change change the position', t => {
+    var sass = renderSass( `
+        @import 'src/main.scss';
+        .test {
+            @include background-image( null, contain, left center );
+        }
+    ` );
+
+    t.is( sass, '.test{background-size:contain;background-repeat:no-repeat;background-position:left center}' );
+} );
